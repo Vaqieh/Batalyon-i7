aea8e18f79c16e3232f4cad9ab46221c430befb1
Vaqieh <vaqiehmuwafvaq@gmail.com>
2024-11-17 23:48:22 +0700

    Updaten terbaru by Vaqieh

--- app/Http/Controllers/DataDiriVaqiehController.php
+++ app/Http/Controllers/DataDiriVaqiehController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\DataDiriVaqieh;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class DataDiriVaqiehController extends Controller
 {
--- app/Models/DataDiriVaqieh.php
+++ app/Models/DataDiriVaqieh.php
@@ -10,8 +10,6 @@ class DataDiriVaqieh extends Model
     use HasFactory;
     protected $table = 'students';
 
-    
-    protected $fillable = [
-        'nama', 'nim', 'prodi', 'alamat', 'email', 'nomor_telepon', 'nama_kelompok', 'foto'
-    ];
+
+    protected $guarded = [];
 }
